25883a899e0760f132cd708fcac6652900456f4f
M0staphaTaha <darsh.taha12382@gmail.com>
2024-04-03 23:30:47 +0200

    RecursiveFactorial

---
+++ Algorithms/dist/Recursion/RecursiveFactorial.js
@@ -0,0 +1,16 @@
+"use strict";
+// RecursiveFactorial //
+/* n! */
+const RecursiveFactorial = (x) => {
+    if (x === 0)
+        return 1;
+    return x * RecursiveFactorial(x - 1);
+};
+console.log(RecursiveFactorial(5)); //120
+console.log(RecursiveFactorial(9)); //362880
+console.log(RecursiveFactorial(6)); //720
+// Calcualtion not dependent on input size - O(1)
+// loop - O(n)
+// nested loop - O(n^2)
+// input size reduced by half - O(logn)
+/* ***************************************************  Big-O = O(1)  ********************************************************** */ 
